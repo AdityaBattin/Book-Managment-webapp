1b741acdd1ff79950bf8058289f75c300b1b818f
AdityaBattin <aditya.g.battin@gmail.com>
2022-08-28 10:58:43 +0530

    refactored the code and added new routes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 //Destructuring a JSON object.....JSON DATA IMPORT
 const { books } = require("./data/books.json");
 const { users } = require("./data/users.json");
+// importing Routes
+const userRouter = require("./routes/users");
+const bookRouter = require("./routes/books");
 const app = express();
 const port = 8081;
 
@@ -12,119 +15,13 @@ app.use(express.json());
 app.get("/", (req, res) => {
   //callBack
   res.status(200).json({
-    massage: "Server is uo and running",
+    massage: "Server is up and running",
   });
 });
 
-/**
- * Route : /users
- * Methord used GET
- * Discrition : to Get all users
- * Access : Public
- * Parameters : NONE
- */
-
-app.get("/users", (req, res) => {
-  //callBack
-  res.status(200).json({
-    success: true,
-    data: users,
-  });
-});
-
-/**
- * Route : /users/${id}
- * Methord used GET
- * Discrition : to Get specific user by ID
- * Access : Public
- * Parameters : ID
- */
-
-app.get("/users/:id", (req, res) => {
-  const { id } = req.params; // getting ID from paramiter
-  const user = users.find((each) => each.id === id); //checking in DATABASE
-  if (!user) {
-    return res.status(404).json({
-      success: false,
-      massage: "user not found",
-    }); // if user not found
-  }
-  res.status(202).json({
-    data: user,
-  });
-});
-
-/**
- * Route : /users
- * Methord used POST
- * Discrition : to Create New Users
- * Access : Public
- * Parameters : NONE
- */
-
-app.post("/users", (req, res) => {
-  const { id, name, surname, email, subscriptionType, subscriptionDate } =
-    req.body;
-
-  // checking if user exist or not
-  const user = users.find((each) => each.id == id);
-
-  //IF user already Exists
-  if (user) {
-    return res.status(404).json({
-      success: false,
-      massage: "USER exists with this ID",
-    });
-  }
-
-  //USER DOES NOT Exist so CREATE one...
-  users.push({
-    id,
-    name,
-    surname,
-    email,
-    subscriptionType,
-    subscriptionDate,
-  });
-
-  return res.status(201).json({
-    success: true,
-    massage: "User Created Successfully",
-    data: users,
-  });
-});
-
-/**
- * Route : /users
- * Methord used PUT
- * Discrition : Upadate existing user
- * Access : Public
- * Parameters : ID
- */
-
-app.put("/users/:id", (req, res) => {
-  const id = req.params;
-  const { data } = req.body;
-
-  const user = users.find((each) => each.id == id);
-
-  if (!user)
-    return res.status(404).json({ success: false, massage: "user not found" });
-
-  const updateUser = users.map((each) => {
-    if (each.id === id) {
-      return {
-        ...each,
-        ...data,
-      };
-    }
-    return res.status(202).json({
-      success: true,
-      massage: "User Created Successfully",
-      data: users,
-    });
-  });
-});
+// to direct requests in there respective routers
+app.use("/users", userRouter);
+app.use("/books/", bookRouter);
 
 //for all the other routes
 app.all("*", (req, res) => {
@@ -135,5 +32,5 @@ app.all("*", (req, res) => {
 
 app.listen(port, () => {
   //callback
-  console.log("Nodejs & Express started on port : ${port}");
+  console.log(`Nodejs & Express started on port : ${port}`);
 });
